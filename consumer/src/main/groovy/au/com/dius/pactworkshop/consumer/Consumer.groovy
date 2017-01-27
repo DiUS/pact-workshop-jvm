d86d65501f21af3b45aa67432f0406d5ed0bd4f9
Ronald Holshausen <uglyog@gmail.com>
2017-01-27 14:24:16 +1100

    Step 2 - add a test to the consumer

--- consumer/src/main/groovy/au/com/dius/pactworkshop/consumer/Consumer.groovy
+++ consumer/src/main/groovy/au/com/dius/pactworkshop/consumer/Consumer.groovy
@@ -1,4 +1,4 @@
 package au.com.dius.pactworkshop.consumer
 
-println new Client().loadProviderJson()
+println new Client().fetchAndProcessData()
 
---
+++ consumer/src/test/groovy/au/com/dius/pactworkshop/consumer/ClientSpec.groovy
@@ -0,0 +1,34 @@
+package au.com.dius.pactworkshop.consumer
+
+import groovyx.net.http.RESTClient
+import spock.lang.Specification
+
+import java.time.ZonedDateTime
+
+class ClientSpec extends Specification {
+
+  private Client client
+  private RESTClient mockHttp
+
+  def setup() {
+    mockHttp = Mock(RESTClient)
+    client = new Client(http: mockHttp)
+  }
+
+  def 'can process the json payload from the provider'() {
+    given:
+    def json = [
+      test: 'NO',
+      date: '2013-08-16T15:31:20+10:00',
+      count: 100
+    ]
+
+    when:
+    def result = client.fetchAndProcessData()
+
+    then:
+    1 * mockHttp.get(_) >> [data: json, success: true]
+    result == [1, ZonedDateTime.parse(json.date)]
+  }
+
+}
