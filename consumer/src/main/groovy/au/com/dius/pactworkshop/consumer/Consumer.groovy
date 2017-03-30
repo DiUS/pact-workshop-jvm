03ca5d0afbb00ec4fe54922082d456396cfd6ef6
Ronald Holshausen <uglyog@gmail.com>
2017-03-31 10:21:15 +1100

    Update the consumer script to take the date as a parameter

--- consumer/src/main/groovy/au/com/dius/pactworkshop/consumer/Consumer.groovy
+++ consumer/src/main/groovy/au/com/dius/pactworkshop/consumer/Consumer.groovy
@@ -2,4 +2,4 @@ package au.com.dius.pactworkshop.consumer
 
 import java.time.LocalDateTime
 
-println new Client('http://localhost:8080').fetchAndProcessData(LocalDateTime.now().toString())
+println new Client('http://localhost:8080').fetchAndProcessData(args ? args[0] : LocalDateTime.now().toString())
